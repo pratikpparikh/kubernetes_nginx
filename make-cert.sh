9f027c4b423aaae8a83b6c39a81cb6ed12d73374
Thom May <thom@may.lt>
2015-01-30 16:14:54 +0000

    strip passphrase

--- make-cert.sh
+++ make-cert.sh
@@ -15,12 +15,19 @@
 # limitations under the License.
 
 openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 \
+  -subj "/O=Kubernetes" \
   -keyout /usr/share/nginx/rootCA.key  -out /usr/share/nginx/rootCA.crt
 
-openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 \
-  -CA /usr/share/nginx/rootCA.crt -CAkey /usr/share/nginx/rootCA.key \
+openssl genrsa -out /tmp/server.key 2048
+openssl rsa -in /tmp/server.key -out /usr/share/nginx/server.key
+
+openssl req -new -key /usr/share/nginx/server.key -out /tmp/host.csr
+
+openssl x509 -req -in /tmp/host.csr -CA /usr/share/nginx/rootCA.crt \
   -config /root/kub-ssl.cnf -extensions v3_ca \
   -subj "/CN=kubernetes.invalid/O=Kubernetes" \
-  -keyout /usr/share/nginx/server.key  -out /usr/share/nginx/server.cert
+  -CAkey /usr/share/nginx/rootCA.key \
+  -CAcreateserial -out /usr/share/nginx/server.crt -days 365
 
-cat /usr/share/nginx/server.cert /usr/share/nginx/rootCA.crt > /usr/share/nginx/server.cert
+
+cat /usr/share/nginx/server.crt /usr/share/nginx/rootCA.crt > /usr/share/nginx/server.cert
